cd3aa12f0f88ed66516ff0e5d03dd724b1ae9979
Bruno Calado <brunocalado@users.noreply.github.com>
2021-07-01 17:28:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Ouija Board for Sequencer
+<p align="center">
+  <img width="300" src="images/guide/usedw.png">
+</p>
+
+<p align="center">
+  <img width="300" src="images/guide/folders.png">
+</p>
+
+# How to Install
+1. Go to modules and use the link: 
+https://raw.githubusercontent.com/brunocalado/ouija-board-for-sequencer/main/module.json
+
+## Instructions
+There is a compendium with journals with instructions.
+
+# Changelog
+You can check changes at [CHANGELOG](CHANGELOG.md)
+
+# License
+Sound: https://99sounds.org/license/
+Image: https://pixabay.com/service/license/
+Designer: Matheus Moreno Mota
+Code: [LICENSE](LICENSE.md)
+
---
+++ macros/GetTokenXY.js
@@ -0,0 +1,51 @@
+// Get Token XY
+
+/*
+
+icon: icons/tools/hand/ruler-steel-grey.webp
+*/
+
+let token;
+
+if (canvas.tokens.controlled[0]===undefined){
+  ui.notifications.error("You must select a token!");    
+} else {
+  token=canvas.tokens.controlled[0];
+  main();
+}
+
+async function main() {
+  let message='';
+  let finalCode = `{ x: ${token.position.x}, y: ${token.position.y} }`;
+  
+  message += `<ul><li>X: <b style="color:red">${token.position.x}</b></li>`;
+  message += `<li>Y: <b style="color:red">${token.position.y}</b></li></ul>`;
+  
+  message += `<b style="color:red" id="#tokenposition">{ ${token.position.x}, ${token.position.y} }</p>`;
+  message += `<p>Copied to clipboard.</p>`;
+
+  
+	let template = message;
+
+	/* view */
+	let form = `
+		<label>Copy this</label>
+		<textarea id="moduleTextArea" rows="5" cols="33">${finalCode}</textarea>
+	`;
+
+	let dialog = new Dialog({
+		title: "Token Data",
+		content: form,
+		buttons: {
+			use: {
+				label: "Copy to Clipboard",
+				callback: () => {
+					let copyText = document.getElementById("moduleTextArea"); /* Get the text field */
+					copyText.select(); /* Select the text field */
+					document.execCommand("copy"); /* Copy the text inside the text field */
+					ui.notifications.notify(`Saved on Clipboard`); /* Alert the copied text */
+				}
+			}
+		}
+	}).render(true);
+}
